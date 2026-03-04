c25b54787be2fddfdcc533bbd11ce991e3fb8675
Teacher <utr.tch@utr.edu.mx>
2026-03-04 14:10:14 -0600

    I did the practice 1

---
+++ practice-1.js
@@ -0,0 +1,12 @@
+// Declare empty array
+let numbers = [];
+
+// Use while loop to generate numbers
+let i = 1;
+while (i < 21) {
+// Insert numbers
+numbers.push(i)
+i++;
+}
+
+console.log(numbers)
